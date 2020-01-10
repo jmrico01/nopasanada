76855f47d5e41159dbff43c218d959aa51e9fb30
Jose M Rico <jmrico01@gmail.com>
2020-01-09 21:45:42 -0500

    finished load kmkv

--- src/main.cpp
+++ src/main.cpp
@@ -36,48 +36,105 @@ internal void FreeFile(const Array<uint8>& outFile, Allocator* allocator)
 	allocator->Free(outFile.data);
 }
 
-struct DynamicStringOrHashTable
+struct KmkvItem
 {
 	bool isString;
+	DynamicArray<char> keywordTag;
 	union {
 		DynamicArray<char>* dynamicStringPtr;
-		HashTable<DynamicStringOrHashTable>* hashTablePtr;
+		HashTable<KmkvItem>* hashTablePtr;
 	};
 };
 
-internal bool LoadKmkv(const char* filePath, HashTable<DynamicStringOrHashTable>* outHashTable)
+template <typename Allocator>
+internal bool LoadKmkvRecursive(Array<char> string, Allocator* allocator,
+	HashTable<KmkvItem>* outHashTable)
 {
-	outHashTable->Clear();
-
-	Array<uint8> kmkvFile;
-	if (!LoadEntireFile(filePath, &standardAllocator_, &kmkvFile)) {
-		fprintf(stderr, "Failed to load file %s\n", filePath);
-		return false;
-	}
-	defer(FreeFile(kmkvFile, &standardAllocator_));
-
-	Array<char> fileString;
-	fileString.size = kmkvFile.size;
-	fileString.data = (char*)kmkvFile.data;
+	const uint64 KEYWORD_MAX_LENGTH = 32;
+	const uint64 VALUE_MAX_LENGTH = KILOBYTES(32);
 	FixedArray<char, KEYWORD_MAX_LENGTH> keyword;
 	FixedArray<char, VALUE_MAX_LENGTH> value;
 	while (true) {
-		int read = ReadNextKeywordValue(fileString, &keyword, &value);
+		int read = ReadNextKeywordValue(string, &keyword, &value);
 		if (read < 0) {
-			fprintf(stderr, "kmkv file keyword/value error (%s)\n", filePath);
+			fprintf(stderr, "kmkv file keyword/value error\n");
 			return false;
 		}
 		else if (read == 0) {
 			break;
 		}
-		fileString.size -= read;
-		fileString.data += read;
+		string.size -= read;
+		string.data += read;
 
-		printf("Keyword: %.*s\n", (int)keyword.size, keyword.data);
-		printf("Value: %.*s\n", (int)value.size, value.data);
+		KmkvItem kmkvValueItem;
+
+		bool keywordHasTag = false;
+		uint64 keywordTagInd = 0;
+		while (keywordTagInd < keyword.size) {
+			if (keyword[keywordTagInd] == '{') {
+				keywordHasTag = true;
+				break;
+			}
+			keywordTagInd++;
+		}
+		if (keywordHasTag) {
+			keywordTagInd++;
+			bool bracketMatched = false;
+			while (keywordTagInd < keyword.size) {
+				if (keyword[keywordTagInd] == '}') {
+					bracketMatched = true;
+					break;
+				}
+				kmkvValueItem.keywordTag.Append(keyword[keywordTagInd++]);
+			}
+			if (!bracketMatched) {
+				fprintf(stderr, "kmkv keyword tag unmatched bracket\n");
+				return false;
+			}
+			if (bracketMatched && keywordTagInd != keyword.size - 1) {
+				fprintf(stderr, "found characters after kmkv keyword tag bracket, keyword %.*s\n",
+					(int)keyword.size, keyword.data);
+				return false;
+			}
+		}
+		if (StringCompare(kmkvValueItem.keywordTag.ToArray(), "kmkv")) {
+			kmkvValueItem.isString = false;
+			// "placement new" - allocate with custom allocator, but still call constructor
+			kmkvValueItem.hashTablePtr = new (allocator->New<HashTable<KmkvItem>>()) HashTable<KmkvItem>();
+			LoadKmkvRecursive(value.ToArray(), allocator, kmkvValueItem.hashTablePtr);
+		}
+		else {
+			kmkvValueItem.isString = true;
+			// "placement new" - allocate with custom allocator, but still call constructor
+			kmkvValueItem.dynamicStringPtr = new (allocator->New<DynamicArray<char>>()) DynamicArray<char>(value.ToArray());
+		}
+		Array<char> keywordArray = keyword.ToArray();
+		if (keywordHasTag) {
+			keywordArray = keywordArray.SliceTo(keywordArray.size - 2 - kmkvValueItem.keywordTag.size);
+		}
+		const HashKey key(keywordArray);
+		outHashTable->Add(key, kmkvValueItem);
 	}
 
 	return true;
+}
+
+template <typename Allocator>
+internal bool LoadKmkv(const char* filePath, Allocator* allocator, HashTable<KmkvItem>* outHashTable)
+{
+	Array<uint8> kmkvFile;
+	if (!LoadEntireFile(filePath, allocator, &kmkvFile)) {
+		fprintf(stderr, "Failed to load file %s\n", filePath);
+		return false;
+	}
+	defer(FreeFile(kmkvFile, allocator));
+
+	Array<char> fileString;
+	fileString.size = kmkvFile.size;
+	fileString.data = (char*)kmkvFile.data;
+
+	outHashTable->Clear();
+	return LoadKmkvRecursive(fileString, allocator, outHashTable);
 }
 
 int main(int argc, char** argv)
@@ -109,10 +166,27 @@ int main(int argc, char** argv)
 		fprintf(stderr, "article get\n");
 	});
 
-	HashTable<DynamicStringOrHashTable> test;
-	if (!LoadKmkv("./data/content/202001/que-paso-venezuela.kmkv", &test)) {
+	HashTable<KmkvItem> test;
+	if (!LoadKmkv("./data/content/202001/que-paso-venezuela.kmkv", &standardAllocator_, &test)) {
 		fprintf(stderr, "LoadKmkv failed\n");
 		return 1;
+	}
+
+	const KmkvItem* contentType = test.GetValue("type");
+	if (contentType->isString) {
+		printf("content type: %.*s\n", (int)contentType->dynamicStringPtr->size, contentType->dynamicStringPtr->data);
+	}
+	const KmkvItem* featured = test.GetValue("featured");
+	if (!featured->isString) {
+		const HashTable<KmkvItem>* featuredKmkv = featured->hashTablePtr;
+		const KmkvItem* pretitle = featuredKmkv->GetValue("pretitle");
+		if (pretitle->isString) {
+			printf("featured pretitle: %.*s\n", (int)pretitle->dynamicStringPtr->size, pretitle->dynamicStringPtr->data);
+		}
+		const KmkvItem* title = featuredKmkv->GetValue("title");
+		if (title->isString) {
+			printf("featured title: %.*s\n", (int)title->dynamicStringPtr->size, title->dynamicStringPtr->data);
+		}
 	}
 
 	printf("Listening on port %d\n", SERVER_PORT);
